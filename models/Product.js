3ae23ea5085ff08fcebb51910d5f76ed01558b48
Brandon Pfluger <bkpfluger@gmail.com>
2022-02-26 14:51:23 +0100

    get product route to work

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ProductSchema = new mongoose.Schema(
     {
         title: {required: true, type: String},
-        description: {required: true, type: String},
+        description: {required: true, type: Array},
         price: {required: true, type: Number},
         weight: {required: false, type: Number},
         slug: {required: false, type: String},
