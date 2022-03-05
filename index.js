73ffec4304dd32d2b613757bb77ee0cdb7279788
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-03-05 13:16:59 +0100

    fix cors issue WIP

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ const cors = require('cors')
 
 app.use(cors())
 
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
-
-const domainsFromEnv = process.env.CORS_DOMAINS || ""
-
-const whitelist = domainsFromEnv.split(",").map(item => item.trim())
 
 app.use(express.static('public'))
 
